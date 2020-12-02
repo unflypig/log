9c11113c3203b4c137a4ab92aa2c1bea9faac71a
unflypig <1085025884@qq.com>
2020-12-02 18:16:42 +0800

    Update log.py

--- python/log.py
+++ python/log.py
@@ -10,7 +10,8 @@ class Logger(object):
         'error':logging.ERROR,
         'crit':logging.CRITICAL
     }#日志级别关系映射
-    def __init__(self,filename,level='info',when='D',backCount=3,fmt='[%(asctime)s]-%(pathname)s(line:%(lineno)d)-%(levelname)s:%(message)s'):
+    #def __init__(self,filename,level='info',when='D',backCount=3,fmt='[%(asctime)s]-%(pathname)s(line:%(lineno)d)-[%(levelname)s]:%(message)s'):
+    def __init__(self,filename,level='info',when='D',backCount=3,fmt='[%(levelname)-8s][%(asctime)s]->%(pathname)s(line:%(lineno)d) %(message)s'):
         self.logger = logging.getLogger(filename)
         format_str = logging.Formatter(fmt)#设置日志格式
         self.logger.setLevel(self.level_relations.get(level))#设置日志级别
